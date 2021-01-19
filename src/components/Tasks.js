3c7f02793f0292e4f2595bead0c3aec77dd05c52
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-01-19 18:13:32 +0100

    State & useState Hook & Global state

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,24 +1,4 @@
-const tasks = [
-	{
-		id: 1,
-		text: 'Doctors Appoinment',
-		day: 'Feb 5th att 2:30pm',
-		reminder: true,
-	},
-	{
-		id: 2,
-		text: 'Meeting at School',
-		day: 'Feb 6th at 1:30pm',
-		reminder: true,
-	},
-	{
-		id: 3,
-		text: 'Food Shopping',
-		day: 'Feb 5th at 2:30pm',
-	},
-];
-
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
 	return (
 		<div>
 			{tasks.map((task) => (
