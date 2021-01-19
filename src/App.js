8ccb30e482d5a3281f0fcfbffc3a47c365c113b2
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-01-19 18:44:24 +0100

    Delete task & prop drilling & Optional message if no tasks

--- src/App.js
+++ src/App.js
@@ -23,10 +23,19 @@ function App() {
 		},
 	]);
 
+	// Delete Task
+	const deleteTask = (id) => {
+		setTasks(tasks.filter((task) => task.id != id));
+	};
+
 	return (
 		<div className="App">
 			<Header />
-			<Tasks tasks={tasks} />
+			{tasks.length > 0 ? (
+				<Tasks tasks={tasks} onDelete={deleteTask} />
+			) : (
+				'No Tasks To Show'
+			)}
 		</div>
 	);
 }
