e5db4ffca59e0b3dac4e6d1e7f90a7a0495a54b7
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-01-19 18:58:22 +0100

    Toggle reminder & conditional stylingToggle reminder & conditional styling

--- src/App.js
+++ src/App.js
@@ -28,11 +28,20 @@ function App() {
 		setTasks(tasks.filter((task) => task.id != id));
 	};
 
+	// Toogle Reminder
+	const toggleReminder = (id) => {
+		setTasks(
+			tasks.map((task) =>
+				task.id === id ? { ...task, reminder: !task.reminder } : task
+			)
+		);
+	};
+
 	return (
 		<div className="App">
 			<Header />
 			{tasks.length > 0 ? (
-				<Tasks tasks={tasks} onDelete={deleteTask} />
+				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
 			) : (
 				'No Tasks To Show'
 			)}
